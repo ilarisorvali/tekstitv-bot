9f02ceb1e15fcb1746ebbfc68847e2128215b827
Ilari Sorvali <42145485+ilarisorvali@users.noreply.github.com>
2024-08-03 22:01:51 +0300

    it responds to hello messages atleast

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 import os
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app_token = os.getenv("SLACK_APP_TOKEN")
+bot_token = os.getenv("SLACK_BOT_TOKEN")
+
+print(app_token)
+print(bot_token)
+
+
+# Initializes your app with your bot token and socket mode handler
+app = App(token=bot_token)
+
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, app_token).start()
